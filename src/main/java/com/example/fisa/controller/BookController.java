bc4ba9c6c0168122bd4a430f561b64b56d6593b3
YeonjiKim0316 <kimyeonji3@gmail.com>
2025-10-28 13:06:33 +0900

    add jpa repository

--- src/main/java/com/example/fisa/controller/BookController.java
+++ src/main/java/com/example/fisa/controller/BookController.java
@@ -1,9 +1,27 @@
 package com.example.fisa.controller;
 
+import com.example.fisa.dao.Book;
+import com.example.fisa.service.BookService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
+
+    // 객체를 주입
+    private final BookService bookService;
+
+    // 생성자
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks(); // loose coupling을 위해서 controller는 service와만 통신합니다.
+    }
 }
--- src/main/java/com/example/fisa/service/BookService.java
+++ src/main/java/com/example/fisa/service/BookService.java
@@ -1,7 +1,21 @@
 package com.example.fisa.service;
 
+import com.example.fisa.dao.Book;
+import com.example.fisa.repository.BookRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
 }

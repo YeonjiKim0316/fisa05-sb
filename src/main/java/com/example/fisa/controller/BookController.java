479512a3752af63e7ba156713be8550b69668288
YeonjiKim0316 <kimyeonji3@gmail.com>
2025-10-29 10:42:42 +0900

    add delete method

--- src/main/java/com/example/fisa/controller/BookController.java
+++ src/main/java/com/example/fisa/controller/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/books")
@@ -22,7 +23,8 @@ public class BookController {
 
     @GetMapping
     public List<Book> getAllBooks() {
-        return bookService.getAllBooks(); // loose coupling을 위해서 controller는 service와만 통신합니다.
+        return bookService.getAllBooks();
+        // loose coupling을 위해서 controller는 service와만 통신합니다.
     }
 
     // 책을 삽입하는 메서드
@@ -30,4 +32,21 @@ public class BookController {
     public Book saveBook(@RequestBody Book book){
         return bookService.saveBook(book);
     }
+
+    // 특정 책을 ID로 조회
+    @GetMapping("/{id}") // @PathVariable
+    public Optional<Book> getBookById(@PathVariable Long id){
+        return bookService.getBookById(id);
+    }
+
+    // 특정 책을 ID로 삭제
+    @DeleteMapping("/{id}")
+    public void deleteBookById(@PathVariable Long id) {
+        bookService.deleteBookById(id);
+    }
+    // 특정 책의 ID와 수정 내용 전부를 받아서 변경
+
+    // 특정 책의 ID와 수정할 내용만 받아서 변경
+
+
 }
--- src/main/java/com/example/fisa/service/BookService.java
+++ src/main/java/com/example/fisa/service/BookService.java
@@ -5,6 +5,7 @@ import com.example.fisa.repository.BookRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
@@ -21,5 +22,14 @@ public class BookService {
 
     public Book saveBook(Book book) {
         return bookRepository.save(book);
+    }
+
+    // Optional<Book> -> null이 있을 때 예외처리해주는 Wrapper Class입니다. {}
+    public Optional<Book> getBookById(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    public void deleteBookById(Long id) {
+        bookRepository.deleteById(id);
     }
 }
